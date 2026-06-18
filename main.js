8fbc76b257b31bf98249b35d00ddfa606d731ca7
wijerett <winnettjeremy@gmail.com>
2026-06-18 09:49:50 -0400

    tested each function

--- main.js
+++ main.js
@@ -2,9 +2,6 @@
 
 
 import { LinkedList } from "./index.js";
-import { Node } from "./index.js";
-
-
 
 
 const newlist = new LinkedList();
@@ -17,7 +14,11 @@ newlist.append("3");
 console.log("Initial:");
 console.log(newlist.toString());
 
-newlist.insertAt(3, 10, 11)
+//newlist.insertAt(2, 10, 11)
 
-console.log("After insert:");
-console.log(newlist.toString());
+const at = newlist.contains("4");
+
+// console.log("After insert:");"
+// console.log(newlist.toString());
+
+console.log(at);
